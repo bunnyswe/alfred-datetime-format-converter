0ef690a3ca13d1528b62d7847e76c828c66fb698
naitiz <zitian.zhang@qunar.com>
2016-02-15 10:49:16 +0800

    ss

--- workflow/process.py
+++ workflow/process.py
@@ -2,14 +2,14 @@
 
 import alfred
 import calendar
-from delorean import utcnow, parse, epoch
+from delorean import utcnow, now, parse, epoch
 
 def process(query_str):
     """ Entry point """
     value = parse_query_value(query_str)
     if value is not None:
         results = alfred_items_for_value(value)
-        xml = alfred.xml(results) # compiles the XML answer
+        xml = alfred.xml(results,len(results)) # compiles the XML answer
         alfred.write(xml) # writes the XML back to Alfred
 
 def parse_query_value(query_str):
@@ -17,7 +17,7 @@ def parse_query_value(query_str):
     try:
         query_str = str(query_str).strip('"\' ')
         if query_str == 'now':
-            d = utcnow()
+            d = now()
         else:
             # Parse datetime string or timestamp
             try:
@@ -36,7 +36,7 @@ def alfred_items_for_value(value):
 
     index = 0
     results = []
-
+    value.shift('Asia/Chongqing')
     # First item as timestamp
     item_value = calendar.timegm(value.datetime.utctimetuple())
     results.append(alfred.Item(
@@ -50,18 +50,36 @@ def alfred_items_for_value(value):
     ))
     index += 1
 
+    item_value = calendar.timegm(value.datetime.timetuple())
+    results.append(alfred.Item(
+        title=str(item_value),
+        subtitle=u'Local Timestamp',
+        attributes={
+            'uid': alfred.uid(index), 
+            'arg': item_value,
+        },
+        icon='icon.png',
+    ))
+    index += 1
+
     # Various formats
     formats = [
+        # 19370101
+        ("%Y%m%d", "%Y%m%d"),
+        # 120027
+        ("%H%M%S", "%H%M%S"),
+        # 19370101 120027
+        ("%Y%m%d %H%M%S", "%Y%m%d %H%M%S"),
+        # 19370101120027000000
+        ("%Y%m%d%H%M%S%f", "%Y%m%d%H%M%S%f"),
         # 1937-01-01 12:00:27
-        ("%Y-%m-%d %H:%M:%S", ''),
-        # 19 May 2002 15:21:36
-        ("%d %b %Y %H:%M:%S", ''), 
-        # Sun, 19 May 2002 15:21:36
-        ("%a, %d %b %Y %H:%M:%S", ''), 
-        # 1937-01-01T12:00:27
-        ("%Y-%m-%dT%H:%M:%S", ''),
+        ("%Y-%m-%d %H:%M:%S", "%Y-%m-%d %H:%M:%S"),
+        # Sun, May 19 2002 15:21:36
+        ("%a, %b %d %Y %H:%M:%S", "%a, %d %b %Y %H:%M:%S"), 
         # 1996-12-19T16:39:57-0800
-        ("%Y-%m-%dT%H:%M:%S%z", ''),
+        ("%Y-%m-%dT%H:%M:%S%z", "%Y-%m-%dT%H:%M:%S%z"),
+        # local
+        ("%x %X", "Local Time"),
     ]
     for format, description in formats:
         item_value = value.datetime.strftime(format)
